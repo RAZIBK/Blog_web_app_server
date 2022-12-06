54812c8e8034fb0a9fcff08cd8d35862ff78e8f9
Razi bk <razibeppukaran@gmail.com.com>
2022-12-06 11:35:53 +0530

    origin changed

--- server.js
+++ server.js
@@ -36,11 +36,12 @@ const server=app.listen(PORT,console.log(`server is running at ${PORT}`));
 const io=require("socket.io")(server,{
     pingTimeout:60000,
     cors:{
-        // origin:'https://blogwebapp.vercel.app',
-        origin:'http://localhost:3000',
+        origin:'https://blogwebapp.vercel.app',
+        
     },
 })
 
+// origin:'http://localhost:3000',
 
 io.on("connection",(socket)=>{
     console.log("connected to socket.io");
